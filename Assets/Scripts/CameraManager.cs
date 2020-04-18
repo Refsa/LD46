b3d70faba563448c1ddf31b7b9ec601a1cf1ed37
refsa <refsa22@gmail.com>
2020-04-18 06:57:54 +0200

    Added camera movement

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    Vector3 previousMousePos = Vector3.zero;
+
+    void Update() 
+    {
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            Vector3 deltaValue = (previousMousePos - Input.mousePosition);
+            deltaValue.z = 0f;
+            transform.position += deltaValue * Time.deltaTime;
+        }
+
+        previousMousePos = Input.mousePosition;
+    }
+}
